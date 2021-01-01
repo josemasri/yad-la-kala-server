c8aff6425c435d3e9743ec21d072127725d22d5b
Jose Masri <josemasri2@gmail.com>
2021-01-01 12:31:20 -0600

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f4094fcd370ef01fc95c94f19da9d7c0'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3662d2bf-0c23-414f-81d3-13f652a5af06'
+};
